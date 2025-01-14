de93a212b859cf6b26a5520b6cdb10b25eaaa7b5
Ana Tot <a.svitlica@vega.local>
2025-01-14 10:30:47 +0100

    add colors

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,27 @@
+"use client";
 import { LogoLink } from "@/components";
 import NavLink from "@/components/Navbar/NavLink";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // Check if the page is scrolled more than a specific threshold
+      setIsScrolled(window.scrollY > 10);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup the event listener on component unmount
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <nav className="flex justify-between items-center px-5 md:px-12 py-5 gap-5 md:gap-0 z-10 fixed w-full">
+    <nav
+      className={`flex justify-between items-center px-5 md:px-12 py-5 gap-5 md:gap-0 z-10 fixed w-full ${isScrolled ? "bg-background shadow-lg" : "bg-transparent"} transition-colors`}
+    >
       <div className="">
         <LogoLink />
       </div>
